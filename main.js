7d846e5bc7c4f5d7460737615c453484c1ecb9d1
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2024-10-22 17:39:22 +0200

    all tasks done

--- main.js
+++ main.js
@@ -16,6 +16,16 @@ class Vehicle{
     }
 }
 
+class FullCar extends Vehicle{
+    constructor(brand,year,color,doors,fuel,speed){
+        super(brand,year,color,doors,fuel);
+        this.speed = speed;
+    }
+    getSpeed(){
+        return `${this.speed}`;
+    }
+}
+
 brand1 = "Fiat";
 year1 = 2019;
 color1 = "blu";
@@ -24,3 +34,12 @@ fuel1 = "gas"
 const vehicle1 = new Vehicle(brand1,year1,color1,doors1,fuel1);
 console.log(vehicle1.getInfo());
 console.log(vehicle1.getCarTime());
+
+brand2 = "byd";
+year2 = 2022;
+color2 = "green";
+doors2 = 5;
+fuel2 = "electric";
+
+const fullcar1 = new FullCar(brand2,year2,color2,doors2,fuel2,190);
+console.log(`${fullcar1.getInfo()} and ${fullcar1.getSpeed()} speed`);
